9e0a02ec661e6e1f92ffdc7c4b115601e0280331
Ties <tg.klappe@gmail.com>
2022-05-13 20:27:22 +0200

    Test for interaction with SSM in CI

--- src/codegen/test.rs
+++ src/codegen/test.rs
@@ -1,5 +1,73 @@
+use crate::codegen::ssm;
 use crate::codegen::ssm::*;
+use crate::parser::*;
+use crate::tc;
 use crate::{ast::*, codegen::ssm::LabelPrefixGenerator};
+
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+
+fn ssm_code_gen_test_helper_output(input_filename: String, expected_output: &str) {
+    let path = Path::new(&input_filename);
+    let input = fs::read_to_string(path).unwrap();
+    let tokens: Vec<Token> = Scanner::new(&input).unwrap().collect();
+    let tokens = Tokens::new(&tokens, "");
+
+    let (rest, mut program) = program_parser(tokens).unwrap();
+    assert!(rest.is_empty());
+
+    assert_eq!(Ok(()), tc::run(&mut program));
+
+    let mut code_gen_env = ssm::LocationEnv::default();
+    let mut heap_offset = 0;
+    let mut prefix_gen = ssm::LabelPrefixGenerator::default();
+
+    let ssm_instructions: Vec<String> = program
+        .instructions(&mut code_gen_env, &mut heap_offset, &mut prefix_gen)
+        .into_iter()
+        .map(|i| i.into())
+        .collect();
+
+    let out_file = format!(
+        "test_{}.out.ssm",
+        path.file_name().unwrap().to_str().unwrap()
+    );
+    let mut file = fs::File::create(out_file.clone()).unwrap();
+
+    for instruction in ssm_instructions {
+        write!(file, "{}\n", instruction).unwrap();
+    }
+
+    let output = Command::new("java")
+        .arg("-jar")
+        .arg("ssm/ssm.jar")
+        .arg("--cli")
+        .arg("--haltonerror")
+        .arg("--file")
+        .arg(format!("{}", out_file))
+        .output()
+        .expect("Failed to execute SSM command");
+
+    if !output.status.success() {
+        println!(
+            "SSM failed. stderr: {}",
+            std::str::from_utf8(&output.stderr).unwrap()
+        );
+    }
+
+    assert_eq!(
+        std::str::from_utf8(&output.stdout).unwrap(),
+        expected_output
+    );
+}
+
+#[test]
+fn ssm_print_output() {
+    let output = "1\na\n(2,b)\n((1,2),(a,b))\n[(1,2),(5,6)]\n[[(1,2),(5,6)],[(1,2),(5,6)]]\n\nmachine halted\n";
+    ssm_code_gen_test_helper_output(String::from("tests/ssm/print.spl"), output);
+}
 
 fn ssm_code_gen_test_helper(node: impl SsmInstructions, expected: Vec<SsmInstruction>) {
     let mut env = LocationEnv::default();
